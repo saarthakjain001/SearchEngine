19db9fdf671cb4355b056c0cbcb91c64583b261a
Saarthak Jain <f20170083@hyderabad.bits-pilani.ac.in>
2019-10-10 11:37:39 +0530

    method to find tf-idf of query

--- tf_idf.py
+++ tf_idf.py
@@ -23,6 +23,27 @@ class tf_idf:
 			iterate=iterate+1
 		return return_vector
 
+	def tf_idf_query(self,Query,global_word,doc_freq,doc_count)
+		word_tf={}
+		for word in Query:
+			if word in word_tf:
+				word_tf[word] = word_tf[word]+1
+			else :
+				word_tf[word] = 1;
+		for word in word_tf:
+			count = word_tf[word]
+			word_tf[word]=float(1+math.log(count,10))
+		return_vector = []
+		for word in global_word:
+			if word in word_count:
+				return_vector.append(word_tf[word])
+			else:
+				return_vector.append(0);
+		for word in return_vector:
+			if(return_vector[word]!=0):
+				return_vector[word]*=math.log(doc_count/doc_freq[word],10)
+		return return_vector
+
 globa = ['Wishing','former', 'cricketer', 'Zaheer', 'Khan', 'on', 'the', 'occasion', 'of', 'his', '41st', 'birthday', 'Team', 'India', 'all-rounder' ,'Hardik', 'Pandya', 'shared', 'a', 'video', 'him','trolls','the fuck']
 test = ['Wishing','former', 'cricketer', 'Zaheer', 'Khan', 'on', 'the', 'occasion', 'of', 'his', '41st', 'birthday', 'Team', 'India', 'all-rounder' ,'Hardik', 'Pandya', 'shared', 'a', 'video', 'of', 'him','Pandya','Pandya','trolls','Zaheer','Khan']
 
